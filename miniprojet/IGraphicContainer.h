4dd2ebd39febe38eea12933fc6aabdfc303020c2
packadal <packadal@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-10-16 13:33:36 +0000

    IGraphicContainer is now a template
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@65 4ddaf586-9532-11dd-8e9d-3bce277bc375

--- miniprojet/IGraphicContainer.h
+++ miniprojet/IGraphicContainer.h
@@ -8,12 +8,14 @@
 #ifndef ICURVECONTAINER_H_
 #define ICURVECONTAINER_H_
 
-class IGraphicContainer{
+#include "IData.h"
+
+template <typename T>class IGraphicContainer{
 	public :
-		virtual void addGraphic(IData*) = 0;
-		virtual void removeGraphic(IData*) = 0;
+		virtual void addGraphic(IData<T>*) = 0;
+		virtual void removeGraphic(IData<T>*) = 0;
 		virtual void removeAll() = 0;
-		virtual void setScale(IData*, float) = 0;
+		virtual void setScale(IData<T>*, float) = 0;
 };
 
 #endif /* IGraphicContainer_H_ */
