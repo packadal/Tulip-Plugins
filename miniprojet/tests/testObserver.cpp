0174625f83f587661c1bd44fd5de58acb47217aa
packadal <packadal@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-10-16 13:16:00 +0000

    changed the observertest so it uses cppunit
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@59 4ddaf586-9532-11dd-8e9d-3bce277bc375

--- miniprojet/tests/testObserver.cpp
+++ miniprojet/tests/testObserver.cpp
@@ -1,42 +1,67 @@
-#include "../Observer.h"
+#ifndef TESTOBSERVER_H
+#define TESTOBSERVER_H
+
+
 #include <iostream>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "../Observer.h"
 #include "../Data.h"
-/*
-class TestObserve : public Observable
-{
-private:
-	int _valeur;
-public:
-	inline void setValeur(int valeur)
-	{
-		_valeur = valeur;
-		notifyObservers();
-	}
-	inline int getValeur() { return _valeur; }
-};
-*/
 
 class TestObservateur : public Observer
 {
 public:
 	TestObservateur(Observable * obs):
-		Observer()
+		Observer(),
+		_data(0)
 	{
 		obs->addObserver(this);
 	}
 	virtual void update(Observable * subject)
 	{
 		std::cout << "L'objet observé a changé ! Il est désormais de taille " << ((Data<int> *)(subject))->size() << "." << std::endl;
+		_data = ((Data<int> *)subject)->size();
 	}
+	inline int getData() const { return _data; }
+private:
+	int _data;
 };
-/*
-int main()
+
+class ObserverTest : public CppUnit::TestCase
 {
-	Data<int> * donneeObservee = new Data<int>;
-	TestObservateur * objetObservateur = new TestObservateur(donneeObservee);
-	donneeObservee->add(5, 2);
-	donneeObservee->add(8, 5);
-	delete donneeObservee;
-	delete objetObservateur;
-}
-*/
+  CPPUNIT_TEST_SUITE(ObserverTest);
+      CPPUNIT_TEST(loadTest);
+  CPPUNIT_TEST_SUITE_END();
+private:
+	IData<float> *_data;
+	TestObservateur * _objetObservateur;
+	Data<int> * _donneeObservee;
+public:
+    void setUp()
+    {
+    	_donneeObservee = new Data<int>;
+		_objetObservateur = new TestObservateur(_donneeObservee);
+    }
+
+    void tearDown()
+    {
+    	delete _donneeObservee;
+		delete _objetObservateur;
+    }
+
+protected:
+    void loadTest()
+    {
+    	CPPUNIT_ASSERT_EQUAL(_objetObservateur->getData(), 0);
+    	_donneeObservee->add(5, 2);
+    	CPPUNIT_ASSERT_EQUAL(_objetObservateur->getData(), 1);
+		_donneeObservee->add(8, 5);
+    	CPPUNIT_ASSERT_EQUAL(_objetObservateur->getData(), 2);
+    }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ObserverTest);
+
+#endif
