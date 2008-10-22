0fb1b237a264c92dd02514c8d69e75df696b8b48
packadal <packadal@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-10-22 12:49:07 +0000

    * renamed IGraphicContainer to IGraphic
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@72 4ddaf586-9532-11dd-8e9d-3bce277bc375

--- miniprojet/IGraphic.h
+++ miniprojet/IGraphic.h
@@ -10,11 +10,10 @@
 
 #include "IData.h"
 
-template <typename T>class IGraphicContainer{
+template <typename T>class IGraphic{
 	public :
-		virtual void addGraphic(IData<T>*) = 0;
-		virtual void removeGraphic(IData<T>*) = 0;
-		virtual void removeAll() = 0;
+		virtual void setGraphic(IData<T>*) = 0;
+		virtual IData<T>* getGraphic() = 0;
 		virtual void setScale(IData<T>*, float) = 0;
 };
 
