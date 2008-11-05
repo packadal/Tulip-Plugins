9cec5e07fe76cb344b23c345ff58c03a0870c249
rudy.nappee@etu.u-bordeaux1.fr <rudy.nappee@etu.u-bordeaux1.fr@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-11-05 17:26:57 +0000

    test add/remove graphics added
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@151 4ddaf586-9532-11dd-8e9d-3bce277bc375

---
+++ miniprojet/tests/AddDeleteTest.cpp
@@ -0,0 +1,60 @@
+#include "AddDeleteTest.h"
+
+
+void AddDeleteTest::setUp()
+{
+	_data = new Data();
+	_data->add(10, 3);
+	_data->add(30, 4);
+	_data->add(40, -10);
+	_data->add(50, 6);
+	_data->add(60, 12);
+
+	_data2 = new Data();
+	_data2->add(10, 5);
+	_data2->add(20, 12);
+
+	_graphic = new CurveGroup(_data);
+	_graphic2 = new HistogramGroup(_data2);
+	_graphic3 = new CurveGroup(_data2);
+	_graphic4 = new HistogramGroup(_data);
+
+
+}
+
+void AddDeleteTest::tearDown()
+{
+}
+
+QList<QGraphicsItemGroup *> AddDeleteTest::fetchGroups(QList<QGraphicsItem *> items) {
+	QList<QGraphicsItemGroup *> groups;
+
+	std::cout << "fin de liste : "<< (*(items.end())) << std::endl;
+	for (QList<QGraphicsItem *>::const_iterator it = items.begin(); it != items.end(); it++) {
+		QGraphicsItemGroup * group = (*it)->group();
+		if (!groups.contains(group) && group != 0)
+			groups.append((*it)->group());
+	}
+	return groups;
+}
+
+void AddDeleteTest::runTest()
+{
+	int nbGroups = fetchGroups(_scene->items()).size();
+
+	CPPUNIT_ASSERT(nbGroups == 0);
+
+	addGraphic(_data, _graphic);
+	CPPUNIT_ASSERT(fetchGroups(_scene->items()).size() == (nbGroups + 1));
+
+	addGraphic(_data2, _graphic2);
+	CPPUNIT_ASSERT(fetchGroups(_scene->items()).size() == (nbGroups + 2));
+
+	addGraphic(_data2, _graphic3);
+	CPPUNIT_ASSERT(fetchGroups(_scene->items()).size() == (nbGroups + 3));
+
+	addGraphic(_data, _graphic4);
+	CPPUNIT_ASSERT(fetchGroups(_scene->items()).size() == (nbGroups + 4));
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(AddDeleteTest);
