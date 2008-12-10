e5f76f2807db381c16a6d8cc9a7af96354da029e
romain33 <romain33@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-12-10 16:57:58 +0000

    ScriptEditor added
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@193 4ddaf586-9532-11dd-8e9d-3bce277bc375

---
+++ ScriptEditor/ScriptEditor.pro
@@ -0,0 +1,10 @@
+TEMPLATE = app
+TARGET = ScriptEditor 
+
+QT        += core gui script
+
+HEADERS   += scripteditor.h
+SOURCES   += main.cpp \
+    scripteditor.cpp
+FORMS     += scripteditor.ui    
+RESOURCES +=
---
+++ ScriptEditor/main.cpp
@@ -0,0 +1,12 @@
+#include "scripteditor.h"
+
+#include <QtGui>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ScriptEditor w;
+    w.show();
+    return a.exec();
+}
