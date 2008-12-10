eb9a12a47eaef31138a34d9b6077a0393b15a7da
packadal <packadal@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-12-10 15:31:48 +0000

    added a test for saving a graph using the script.
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@187 4ddaf586-9532-11dd-8e9d-3bce277bc375

---
+++ ScriptTulip/test/saveGraphTest.h
@@ -0,0 +1,29 @@
+#ifndef IDATA_TESTCASE_H
+#define IDATA_TESTCASE_H
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <tulip/Graph.h>
+class QScriptEngine;
+
+class saveGraphTest : public CppUnit::TestCase
+{
+  CPPUNIT_TEST_SUITE(saveGraphTest);
+      CPPUNIT_TEST(saveTest);
+  CPPUNIT_TEST_SUITE_END();
+private:
+	tlp::Graph* _graph;
+	QScriptEngine* _engine;
+	std::string _savedFile;
+public:
+    void setUp();
+    void tearDown();
+
+protected:
+    void saveTest();
+
+};
+
+#endif
+
