81c1cb7b1148d27e732dc77b3139f16d730ba71d
rudy.nappee@etu.u-bordeaux1.fr <rudy.nappee@etu.u-bordeaux1.fr@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2008-12-18 14:28:13 +0000

    GetNodeValueTest done
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@227 4ddaf586-9532-11dd-8e9d-3bce277bc375

---
+++ ScriptTulip/test/GetNodeValueTest.cpp
@@ -0,0 +1,49 @@
+#include "GetNodeValueTest.h"
+#include <tulip/Graph.h>
+
+#include "QGraph.h"
+#include "QProperty.h"
+#include "QNode.h"
+#include "utilsTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(GetNodeValueTest);
+
+
+void GetNodeValueTest::setUp()
+{
+	_engine = new TulipScriptEngine();
+	_engine->addScriptFunction(storeString, "storeString");
+
+
+}
+
+void GetNodeValueTest::tearDown()
+{
+	delete _engine;
+	delete _string;
+}
+
+void GetNodeValueTest::invokeTest()
+{
+	tlp::Graph* graph = tlp::newGraph();
+	QGraph qgraph(_engine,graph);
+	QNode node ;
+	qgraph.addNode(&node);
+	QProperty property(graph->getProperty("viewColor"));
+	QString value("(10,11,12)");
+	property.setNodeStringValue(&node,value.toStdString());
+
+
+	_engine->addQObject(&property,QString::fromStdString("property"));
+	_engine->addQObject(&node,QString::fromStdString("node"));
+
+	_engine->evaluate("var value = property.getNodeStringValue(node); storeString(value);");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+
+	CPPUNIT_ASSERT(value == _string);
+
+}
+
