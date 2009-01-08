8ba795b8ff62646c5f34d9a2613f6e535d528c81
packadal <packadal@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2009-01-08 15:01:29 +0000

    added a typesystem, global.h and ant build file for qtJambi. Pathes are hard-coded, but this should change soon. The qtjambi jar are neededd in order to compile this. A how-to should come on the wiki soon
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@249 4ddaf586-9532-11dd-8e9d-3bce277bc375

---
+++ ScriptTulip/global.h
@@ -0,0 +1,9 @@
+#include "QEdge.h"
+#include "QGraph.h"
+#include "QIterator.h"
+#include "QNode.h"
+#include "QProperty.h"
+#include "Scriptmacros.h"
+#include "TulipScriptEngine.h"
+
+#include <QtCore/QChildEvent>
