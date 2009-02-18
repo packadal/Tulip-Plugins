a397998ea118377c23c2e7cc9ad0ee7602999330
quentin.enard@wanadoo.fr <quentin.enard@wanadoo.fr@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2009-02-18 16:29:25 +0000

    Headers for Concrete Properties
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@272 4ddaf586-9532-11dd-8e9d-3bce277bc375

---
+++ ScriptTulip/QIntegerProperty.h
@@ -0,0 +1,41 @@
+#ifndef QINTEGERPROPERTY_H_
+#define QINTEGERPROPERTY_H_
+
+#include <QObject>
+
+#include <tulip/IntegerProperty.h>
+
+class QIntegerProperty: public QProperty {
+
+Q_OBJECT
+
+public:
+
+	QIntegerProperty(QGraph *);
+	QIntegerProperty(tlp::IntegerProperty*);
+	virtual ~QIntegerProperty();
+	tlp::IntegerProperty* asProperty();
+
+public slots:
+
+	// Bindings from AbstractProperty
+	int getNodeDefaultValue();
+	int getEdgeDefaultValue();
+	int getNodeValue(const QNode* n);
+	int getEdgeValue(const QEdge* e);
+	void setNodeValue(const QNode* n, int v);
+	void setEdgeValue(const QEdge* e, int v);
+	void setAllNodeValue(int v);
+	void setAllEdgeValue(int v);
+
+	// Specific methods
+	int getNodeMin();
+	int getNodeMax();
+	int getEdgeMin();
+	int getEdgeMax();
+
+private:
+
+	tlp::IntegerProperty* _property;
+
+#endif /* QINTEGERPROPERTY_H_ */
