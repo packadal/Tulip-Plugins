d532658b2826319a4e7a612484356359f269be9a
packadal <packadal@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2009-03-04 13:21:14 +0000

    reworked the macros so only one is needed
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@280 4ddaf586-9532-11dd-8e9d-3bce277bc375

--- ScriptTulip/QStdSet.h
+++ ScriptTulip/QStdSet.h
@@ -8,52 +8,29 @@
 #ifndef QEDGESET_H_
 #define QEDGESET_H_
 
-#include <QtCore/QSet>
+#define QSTDSET(Type, tlpType) QSTDCONTAINER(Set, std::set, insert, Type, tlpType)
 
-#define QSTDSET(Type, tlpType) class Q##Type##Set: QSet<Q##Type*>\
+#define QSTDVECTOR(Type, tlpType) QSTDCONTAINER(Vector, std::vector, push_back, Type, tlpType)
+
+#define QSTDCONTAINER(CONTAINER_NAME, CONTAINER_TYPE, INSERT_FUNCTION, Type, tlpType) class Q##Type##CONTAINER_NAME: QSet<Q##Type*>\
 {\
 public:\
 \
-	Q##Type##Set(std::set<tlpType> s) {\
-		std::set<tlpType>::iterator it = s.begin();\
+	Q##Type##CONTAINER_NAME(CONTAINER_TYPE<tlpType> s) {\
+		CONTAINER_TYPE<tlpType>::iterator it = s.begin();\
 		while(it != s.end())\
 			insert(new Q##Type(*it));\
 		}\
 \
-	std::set<tlpType> as##Type() {\
-		std::set<tlpType> ret;\
+	CONTAINER_TYPE<tlpType> as##Type() {\
+		CONTAINER_TYPE<tlpType> ret;\
 		QSet<Q##Type*>::iterator it = this->begin();\
 		while(it != this->end())\
-			ret.insert((*it)->as##Type());\
+			ret.INSERT_FUNCTION((*it)->as##Type());\
 		return ret;\
 	}\
 \
-	std::set<tlpType> asGraph() {\
-		return as##Type();\
-	}\
-}
-
-#include <QtCore/QVector>
-
-#define QSTDVECTOR(Type, tlpType) class Q##Type##Vector: QVector<Q##Type*>\
-{\
-public:\
-\
-	Q##Type##Vector(std::vector<tlpType> s) {\
-		std::vector<tlpType>::iterator it = s.begin();\
-		while(it != s.end())\
-			push_back(new Q##Type(*it));\
-		}\
-\
-	std::vector<tlpType> as##Type() {\
-		std::vector<tlpType> ret;\
-		QVector<Q##Type*>::iterator it = this->begin();\
-		while(it != this->end())\
-			ret.push_back((*it)->as##Type());\
-		return ret;\
-	}\
-\
-	std::vector<tlpType> asGraph() {\
+	CONTAINER_TYPE<tlpType> asGraph() {\
 		return as##Type();\
 	}\
 }
