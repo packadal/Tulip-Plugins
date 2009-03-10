7725244e6c8be1cc13d18d00cecb70c903f976d3
quentin.enard@wanadoo.fr <quentin.enard@wanadoo.fr@4ddaf586-9532-11dd-8e9d-3bce277bc375>
2009-03-10 10:45:56 +0000

    More property tests
    git-svn-id: https://tulip-plugins.googlecode.com/svn/trunk@295 4ddaf586-9532-11dd-8e9d-3bce277bc375

--- ScriptTulip/test/QPropertyTest.cpp
+++ ScriptTulip/test/QPropertyTest.cpp
@@ -30,21 +30,98 @@ void QPropertyTest::setNodeValueTest()
 {
 	QGraph qgraph;
 	QNode* node = qgraph.addNode();
-	QProperty* property = qgraph.getColorProperty("viewColor");
-	property->setNodeStringValue(node,"(0,0,0,255)");
-	QString value("(10,11,12,255)");
+	QProperty* property0 = qgraph.getColorProperty("viewColor");
+	QProperty* property1 = qgraph.getGraphProperty("viewGraph");
+	QProperty* property2 = qgraph.getDoubleProperty("viewDouble");
+	QProperty* property3 = qgraph.getLayoutProperty("viewLayout");
+	QProperty* property4 = qgraph.getStringProperty("viewString");
+	QProperty* property5 = qgraph.getIntegerProperty("viewInteger");
+	QProperty* property6 = qgraph.getSizeProperty("viewSize");
+	QProperty* property7 = qgraph.getBooleanProperty("viewBoolean");
 
-	_engine->addQObject(property,QString::fromStdString("property"));
+	_engine->addQObject(property0,QString::fromStdString("property0"));
+	_engine->addQObject(property1,QString::fromStdString("property1"));
+	_engine->addQObject(property2,QString::fromStdString("property2"));
+	_engine->addQObject(property3,QString::fromStdString("property3"));
+	_engine->addQObject(property4,QString::fromStdString("property4"));
+	_engine->addQObject(property5,QString::fromStdString("property5"));
+	_engine->addQObject(property6,QString::fromStdString("property6"));
+	_engine->addQObject(property7,QString::fromStdString("property7"));
+
 	_engine->addQObject(node,QString::fromStdString("node"));
 
-	_engine->evaluate("property.setNodeStringValue(node, \""+ value +"\")");
+	QString value("(10,11,12,255)");
+	_engine->evaluate("property0.setNodeStringValue(node, \""+ value +"\")");
 	if(_engine->hasUncaughtException())
 	{
 			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
 	}
-	QString result = property->getNodeStringValue(node);
-
+	QString result = property0->getNodeStringValue(node);
 	CPPUNIT_ASSERT(value == result);
+
+	value="";
+	_engine->evaluate("property1.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property1->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
+	value="2.1";
+	_engine->evaluate("property2.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property2->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
+	value="(100,10,1000)";
+	_engine->evaluate("property3.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property3->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
+	value="a standard test for stringzz";
+	_engine->evaluate("property4.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property4->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
+	value="10";
+	_engine->evaluate("property5.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property5->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
+	value="(100,10,1000)";
+	_engine->evaluate("property6.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property6->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
+	value="false";
+	_engine->evaluate("property7.setNodeStringValue(node, \""+ value +"\")");
+	if(_engine->hasUncaughtException())
+	{
+			CPPUNIT_FAIL(qPrintable(_engine->uncaughtException().toString()));
+	}
+	result = property7->getNodeStringValue(node);
+	CPPUNIT_ASSERT(value == result);
+
 
 }
 
